9e43859d10aa4739a8de2ce12307d9dfc4c66691
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-10-31 18:25:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ var code = "var Module=typeof Module!==\"undefined\"?Module:{};Module=(function(
         console.log('loaded chess engine');
     }
 
-    var lastValue = 10;
+    var lastValue = 25;
     myFunctions.runChessEngine = function(depth){
         //var fen = myFunctions.rescan();
         var fen = $('wc-chess-board')[0].game.getFEN();
@@ -302,92 +302,6 @@ var code = "var Module=typeof Module!==\"undefined\"?Module:{};Module=(function(
             myFunctions.runChessEngine(lstValue);
         }
     }
-
-    document.onkeydown = function(e) {
-        switch (e.keyCode) {
-            case 81:
-                myFunctions.runChessEngine(1);
-                break;
-            case 87:
-                myFunctions.runChessEngine(2);
-                break;
-            case 69:
-                myFunctions.runChessEngine(3);
-                break;
-            case 82:
-                myFunctions.runChessEngine(4);
-                break;
-            case 84:
-                myFunctions.runChessEngine(5);
-                break;
-            case 89:
-                myFunctions.runChessEngine(6);
-                break;
-            case 85:
-                myFunctions.runChessEngine(7);
-                break;
-            case 73:
-                myFunctions.runChessEngine(8);
-                break;
-            case 79:
-                myFunctions.runChessEngine(9);
-                break;
-            case 80:
-                myFunctions.runChessEngine(10);
-                break;
-            case 65:
-                myFunctions.runChessEngine(11);
-                break;
-            case 83:
-                myFunctions.runChessEngine(12);
-                break;
-            case 68:
-                myFunctions.runChessEngine(13);
-                break;
-            case 70:
-                myFunctions.runChessEngine(14);
-                break;
-            case 71:
-                myFunctions.runChessEngine(15);
-                break;
-            case 72:
-                myFunctions.runChessEngine(16);
-                break;
-            case 74:
-                myFunctions.runChessEngine(17);
-                break;
-            case 75:
-                myFunctions.runChessEngine(18);
-                break;
-            case 76:
-                myFunctions.runChessEngine(19);
-                break;
-            case 90:
-                myFunctions.runChessEngine(20);
-                break;
-            case 88:
-                myFunctions.runChessEngine(21);
-                break;
-            case 67:
-                myFunctions.runChessEngine(22);
-                break;
-            case 86:
-                myFunctions.runChessEngine(23);
-                break;
-            case 66:
-                myFunctions.runChessEngine(24);
-                break;
-            case 78:
-                myFunctions.runChessEngine(25);
-                break;
-            case 77:
-                myFunctions.runChessEngine(26);
-                break;
-            case 187:
-                myFunctions.runChessEngine(100);
-                break;
-        }
-    };
 
     myFunctions.spinner = function() {
         if(isThinking == true){
@@ -436,7 +350,10 @@ var code = "var Module=typeof Module!==\"undefined\"?Module:{};Module=(function(
 <input type="checkbox" id="autoMove" name="autoMove" value="false">
 <label for="autoMove"> Enable auto move</label><br>
 <input type="number" id="timeDelay" name="timeDelay" min="0.1" value=0.1>
-<label for="timeDelay">Auto Run Delay (Seconds)</label></div>`
+<label for="timeDelay">Auto Run Delay (Seconds)</label>
+<input type="number" id="depth" name="timeDelay" min="0.1" value=0.1>
+<label for="timeDelay">Set Depth (Must be below 8000 and more than 0)</label>
+</div>`
             div.innerHTML = content;
             div.setAttribute('style','background-color:white; height:auto;');
             div.setAttribute('id','settingsContainer');
